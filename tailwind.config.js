0f6b09c371158d49642c1db4ce37f4ca2cbf8381
Supanan Chotteratat <supanan.c@mail.kmutt.ac.th>
2021-04-28 15:52:36 +0700

    test git

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
 		extend: {
 			colors: {
 				deepBlue: "#13315C",
+				redc: "#C62121",
+				bluec: "#003DBB",
+				greenc: "#32612D",
+				greyc: "#848484",
+				orangec: "#FA6E09",
+				yellowc: "#FFDB21",
+				greyBorderc: "#6E6B6B"
 			},
 			fontFamily: {
 				dosis: ['"Dosis"', "sans-serif"],
@@ -13,7 +20,10 @@ module.exports = {
 		},
 	},
 	variants: {
-		extend: {},
+		extend: {
+			backgroundColor: ['checked'],
+      		borderColor: ['checked'],
+		},
 	},
 	plugins: [],
 };
