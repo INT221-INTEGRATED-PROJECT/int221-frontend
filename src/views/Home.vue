8271ea503215a34761c2f02af5eddca7f5ca6b6e
Qwanjai <d_khonjai@hotmail.com>
2021-04-28 11:08:55 +0700

    [ADD] Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,41 @@
 <template>
-	<!-- <div class="h-auto">
-		<img src="../assets/car1.jpg" class="w-screen bg-center bg-cover " />
-		<img src="../assets/car2.jpg" class="w-screen bg-center bg-cover " />
+	<div class="">
+		<img src="../assets/car2.jpg" class="w-screen bg-center bg-cover" />
+		<div class="absolute bottom-1/2 left-5  px-5  text-6xl text-white uppercase">
+			<h1 class="font-bold ml-12 ">911 turbos</h1>
+			<h1>high performance</h1>
+		</div>
 	</div>
-	<div class="bg-blue-400 h-60 px-14">
-		<div class="bg-red-400 h-44 "></div>
-	</div> -->
-	<div class="m-6   flex flex-row space-x-4 justify-center bg-blue-500">
-		<div v-for="b in brandCars" :key="b.brand" class="flex flex-row">
-			<div class="baseBrandBlock">
-				<div class="row-span-3   px-4">
-					<img :src="b.imgSrc" class="w-3/5" />
+	<div class="px-14 mt-10">
+		<div class="  p-5">
+			<h2 class="text-3xl font-semibold mb-5">Cayenne Design highlights.</h2>
+			<div>
+				<img src="@/assets/porsche_cayenne.jpg" class="w-full mb-5" />
+			</div>
+			<div class="rowAlignment">
+				<div v-for="c in caynneDescription" :key="c.title" class="flex flex-row">
+					<div class="grid grid-rows-4  shadow-lg border border-gray-400">
+						<span class="p-3 text-xl font-semibold">{{ c.title }}</span>
+						<span class="row-span-3  px-3 text-lg pb-6 pt-3">
+							<p>{{ c.des }}</p></span
+						>
+					</div>
 				</div>
-				<h1>{{ b.brand }}</h1>
 			</div>
 		</div>
 	</div>
+	<!-- <div class="m-6 py-5  flex flex-row space-x-4 justify-center bg-blue-500 h-auto ">
+		<div v-for="b in brandCars" :key="b.brand" class="flex flex-row">
+			<div class="baseBrandBlock">
+				<div class="row-span-3   px-4 bg-red-300 rounded-xl pb-4">
+					<img :src="b.imgSrc" class="block mr-auto ml-auto " />
+				</div>
+				<div class=" rounded-xl">
+					<h1>{{ b.brand }}</h1>
+				</div>
+			</div>
+		</div>
+	</div> -->
 	<!-- <img src="@/assets/logo/audi_logo.png" />
 	<div></div> -->
 </template>
@@ -28,14 +48,27 @@ export default {
 	data() {
 		return {
 			// message: "Test",
-			brandCarSorted: "",
-			brandCars: [
-				{ brand: "Audi", imgSrc: require("@/assets/logo/audi_logo.png") },
-				{ brand: "Ferrari", imgSrc: require("@/assets/logo/ferrari_logo.png") },
-				{ brand: "Lamborgini", imgSrc: require("@/assets/logo/lamborghini_logo.png") },
-				{ brand: "Maserati", imgSrc: require("@/assets/logo/maserati_logo.png") },
-				{ brand: "Posrche", imgSrc: require("@/assets/logo/porsche_logo.png") },
-				{ brand: "Rolls Royce", imgSrc: require("@/assets/logo/rollsroyce_logo.png") },
+			caynneDescription: [
+				{
+					title: "Unmistakeable Porsche DNA",
+					des:
+						"All Cayenne  models clearly express their heritage. Thanks to their heavily emphasised front wings, a bonnet withpower domes and characterictic three- part air intakes",
+				},
+				{
+					title: "Design ",
+					des:
+						"What unites us all is our enthusiasm for the sports car experience. This begins even before we start the engine  with our first glimpse of the dynamic proportions of the Cayenne models, and  reveal their Porsche DNA. ",
+				},
+				{
+					title: "E-Hybrid",
+					des:
+						"Our Cayenne models demonstrate that: we refer to  e-mobility as E-Performance, because sustainability coupled with driving dynamics,bring a joyful smile to the faces of sports car enthusiasts.",
+				},
+				{
+					title: "Special e-driving modes ",
+					des:
+						"In our E-Hybrid models, the mode switch and PCM are used to select driving modes specially designed for their drive.You can unlock the full potential of your Cayenne or Cayenne Coupé at all times. ",
+				},
 			],
 		};
 	},
