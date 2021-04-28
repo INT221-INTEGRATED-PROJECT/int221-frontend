4fe16bcf26a7c5dca9171cdc1ed8252fcbba6142
Qwanjai <d_khonjai@hotmail.com>
2021-04-29 00:39:31 +0700

    [EDIT] brandblock size

--- src/components/BrandBlock.vue
+++ src/components/BrandBlock.vue
@@ -1,12 +1,12 @@
 <template>
 	<!-- <h1>This is brandblock component</h1> -->
-	<div class="m-6 py-5  flex flex-row space-x-4 justify-center bg-blue-500 h-auto ">
+	<div class="m-6 py-5  flex flex-row space-x-4 justify-center h-auto ">
 		<div v-for="b in brandCars" :key="b.brand" class="flex flex-row">
-			<div class="baseBrandBlock">
-				<div class="row-span-3   px-4 bg-red-300 rounded-xl pb-4">
-					<img :src="b.imgSrc" class="block mr-auto ml-auto " />
+			<div class="baseBrandBlock ">
+				<div class="row-span-3   px-4  rounded-xl py-4">
+					<img :src="b.imgSrc" class="block mr-auto ml-auto w-28 " />
 				</div>
-				<div class=" rounded-xl">
+				<div class=" rounded-xl ">
 					<h1>{{ b.brand }}</h1>
 				</div>
 			</div>
