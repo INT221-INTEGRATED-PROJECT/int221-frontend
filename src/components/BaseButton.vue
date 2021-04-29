ec46f8eb85b2649ba1c7c9658c88f74fae183c8e
Qwanjai <d_khonjai@hotmail.com>
2021-04-30 00:07:51 +0700

    [EDIT] add product,css classes

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>
-        <button class="text-deepBlue bg-white px-3 py-1 mt-1 border border-deepBlue w-36 h-14 font-medium text-xl rounded-md"></button> 
-    </div>
+	<div>
+		<button
+			class="text-deepBlue bg-white px-3 py-1 mt-1 border border-deepBlue w-36 h-14 font-medium text-xl rounded-md"
+		></button>
+	</div>
 </template>
+<script>
+export default {
+	name: "BaseButton",
+};
+</script>
--- src/components/BaseForm.vue
+++ src/components/BaseForm.vue
@@ -1,16 +1,16 @@
 <template>
-	<div class="mr-auto ml-auto bg-blue-200 h-screen w-3/4">
+	<div class="mr-auto ml-auto  h-screen w-3/4">
 		<form>
 			<div class="container w-4/5 p-8 mx-auto mt-10 rounded-lg">
-				<div id="twoPara" class="grid grid-cols-2 gap-4">
+				<div class="grid grid-cols-2 gap-4">
 					<div class="formAlignment">
-						<label class="leading-7">Car Name</label>
-						<input type="text" id="pdName" placeholder="car name" class="px-4 py-4 mb-4 rounded-md text-gray-500" />
+						<label>Car Name</label>
+						<input type="text" id="pdName" placeholder="Car Name" class="px-4 py-4 mb-4 rounded-md text-gray-500" />
 					</div>
 					<!-- ml-2 -->
 					<div class="formAlignment">
-						<label class="leading-7">Brand</label>
-						<select id="brand" class="px-4 py-4  rounded">
+						<label>Brand</label>
+						<select id="brand" class="px-4 py-4  rounded border border-skyBlue">
 							<option value="audi">Audi</option>
 							<option value="ferrari">Ferrari</option>
 							<option value="lamborghini">Lamborghini</option>
@@ -19,39 +19,48 @@
 							<option value="rollsroyce">Rolls Royce</option>
 						</select>
 					</div>
+				</div>
+				<div class="grid grid-cols-3 gap-4">
 					<div class="formAlignment">
-						<label class="leading-7">Release Date</label>
-						<input type="date" id="releaseDate" placeholder="mm//dd//yyyy" class="px-4 py-4 mb-4 rounded-md" />
+						<label>Release Date</label>
+						<input type="date" id="releaseDate" placeholder="MM//DD//YYYY" class="px-4 py-4 mb-4 rounded-md" />
 					</div>
 					<div class="formAlignment">
-						<label class="leading-7">Price</label>
+						<label>Price</label>
 						<input type="text" id="price" placeholder="USD $ " class="px-4 py-4  rounded-md" />
+					</div>
+					<div id="warranty" class="formAlignment">
+						<label>Warranty</label>
+						<select id="warranty" class="px-4 py-4  rounded border border-skyBlue">
+							<option value="1">1 Year</option>
+							<option value="2">2 Year</option>
+							<option value="3">3 Year</option>
+							<option value="4">4 Year</option>
+							<option value="5">5 Year</option>
+						</select>
 					</div>
 				</div>
 				<div id="cardescript" class="formAlignment">
-					<label class="leading-7">Car Description</label>
-					<textarea rows="5" cols="5" id="carDes" class="p-9  rounded-md " />
+					<label>Car Description</label>
+					<textarea rows="3" cols="5" id="carDes" class="p-9  rounded-md  border border-skyBlue" />
 				</div>
-				<div>
-					<label class="leading-7">Image and Color</label>
-					<!-- <UploadImages class="mb-10" /> -->
+				<div class="mb-5">
+					<label class="mr-5">Colors </label>
+					<input type="checkbox" id="colorPick" name="yellow" value="yellow" />
+					<span class="colorSpan bg-yellowc" />
+					<input type="checkbox" id="colorPick" name="red" value="red" />
+					<span class="colorSpan bg-redc" />
+					<input type="checkbox" id="colorPick" name="blue" value="blue" />
+					<span class=" colorSpan bg-bluec" />
+					<input type="checkbox" id="colorPick" name="green" value="green" />
+					<span class=" colorSpan bg-greenc" />
+					<input type="checkbox" id="colorPick" name="black" value="black" />
+					<span class=" colorSpan bg-black" />
+					<input type="checkbox" id="colorPick" name="white" value="white" />
+					<span class=" colorSpan bg-white border-black" />
 				</div>
-
-				<div id="warranty">
-					<span class="leading-7">Warranty</span>
-					<div class="mb-1 mt-1">
-						<input type="radio" id="1year" value="1year" />
-						<span class="leading-7 ml-4">1year</span>
-					</div>
-					<div class="mb-1">
-						<input type="radio" id="2year" value="2year" />
-						<span class="leading-7 ml-4">2year</span>
-					</div>
-					<div class="mb-1">
-						<input type="radio" id="3year" value="3year" />
-						<span class="leading-7 ml-4">3year</span>
-					</div>
-				</div>
+				<label>Upload Image </label>
+				<input id="file-input" type="file" class="border border-white" />
 			</div>
 		</form>
 	</div>
