3d58385982f43b87ced769a5feacdf3746cae173
Qwanjai <d_khonjai@hotmail.com>
2021-04-30 22:55:56 +0700

    [ADD] global component (basebutton)

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./index.css";
+import BaseButton from "./components/BaseButton";
 
-createApp(App)
-	.use(router)
-	.mount("#app");
+// createApp(App)
+// 	.use(router)
+// 	.mount("#app");
+const app = createApp(App);
+app.component("base-button", BaseButton);
+app.use(router).mount("#app");
