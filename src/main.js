07fc5fa90b7b027f954bbe45ccdc56cee7c013cb
Qwanjai <d_khonjai@hotmail.com>
2021-05-05 21:38:35 +0700

    Ass axios

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import "./index.css";
 import BaseButton from "./components/BaseButton";
+// import Vue from "vue";
+// import axios from "axios";
+// import VueAxios from "vue-axios";
 
+// Vue.use(VueAxios, axios)
 // createApp(App)
 // 	.use(router)
 // 	.mount("#app");
